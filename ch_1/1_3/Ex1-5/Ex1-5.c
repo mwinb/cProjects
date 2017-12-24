cf3e77bfd0c3cb1d2cd781f19e6f58b66cc5d1d9
mwinb <mwinberry0101@gmail.com>
2017-12-23 21:54:50 -0800

    Created Ex1-5 reversing fahrtoCel from 300 to 0, using for loop

--- ch_1/1_3/Ex1-5/Ex1-5.c
+++ ch_1/1_3/Ex1-5/Ex1-5.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/* print Fahrenheit-Celsius table using for loop */
+/* print fahrenheit-celsius table using for loop
+   starting at fahr = 300, 280, ..., 0 */
 main()
 {
     int fahr;
     printf("%s\t %s\n", "Fahrenheit", "Celsius");
-    for (fahr = 0; fahr <=300; fahr = fahr + 20)
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
         printf("%10d\t %7.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
