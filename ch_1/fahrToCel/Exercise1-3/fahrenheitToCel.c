389dc44d9af1d6d87cc3024b899c7933dd234ca0
mwinb <mwinberry0101@gmail.com>
2017-12-23 21:23:22 -0800

    Created directories for Ex. 1-3 and 1-4. Completed 1-3 by adding a printf statement prior to the while loop.

---
+++ ch_1/fahrToCel/Exercise1-3/fahrenheitToCel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* print fahrenheit-Celsius table
+	for fahr = 0, 20, ....,  300 */
+main()
+{
+     float fahr, celsius;
+	int lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	
+    printf("%s\t %s\n", "Fahrenheit", "Celcius");
+	fahr = lower;
+	while (fahr <= upper) {
+            celsius = 5.0 * (fahr - 32.0) / 9.0;
+            printf("%10.0f\t %7.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
